b12c9430a1f9b196cc050c75bc0b7516e18e6214
Ronaldo Orling <ronaldo@orling.com.br>
2023-06-15 02:25:04 +0100

    Projeto finalizado

--- src/infrastructure/api/__tests__/customer.e2e.spec.ts
+++ src/infrastructure/api/__tests__/customer.e2e.spec.ts
@@ -84,5 +84,15 @@ describe("E2E test for customer", () => {
         expect(customer2.name).toBe("Jane");
         expect(customer2.address.street).toBe("Street 2");
 
+        
+        const listResponseXML = await request(app)
+        .get("/customer")
+        .set("Accept", "application/xml")
+        .send();
+
+        expect(listResponseXML.status).toBe(200); 
+        expect(listResponseXML.text).toContain(`<?xml`);   
+     
+
     });
 });
--- src/infrastructure/api/routes/customer.route.ts
+++ src/infrastructure/api/routes/customer.route.ts
@@ -2,6 +2,7 @@ import express, {Request, Response} from 'express';
 import CreateCustomerUseCase from '../../../usecase/customer/create/create.customer.usecase';
 import CustomerRepository from '../../customer/repository/sequelize/customer.repository';
 import ListCustomerUseCase from '../../../usecase/customer/list/list.customer.usercase';
+import CustomerPresenter from '../presenters/customer.presenter';
 
 export const customerRoute = express.Router();
 
@@ -34,7 +35,12 @@ customerRoute.get('/', async (req: Request, res: Response) => {
 
     try {
         const output = await usecase.execute({});
-        res.send(output);
+        res.format({
+            json: async () => res.send(output),
+            xml: async () => res.send(CustomerPresenter.toXML(output)),
+        })
+        
+      //  res.send(output);
 
 
     } catch(err) {
